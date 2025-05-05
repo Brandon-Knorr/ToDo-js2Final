e1b7edecd6a23d563ff5d86390d4681a87ff0c0b
Brandon-Knorr <bknorr@my.wctc.edu>
2025-05-04 19:32:52 -0500

    0.07 create app header

--- vuetify-todo/src/components/AppHeader.vue
+++ vuetify-todo/src/components/AppHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <v-app-bar>
-      <v-app-bar-title>YORELIST</v-app-bar-title>
-
+    <v-app-bar :elevation="2" rounded class="">
       <template v-slot:prepend>
-        <v-icon>mdi-feather</v-icon>
+        <v-app-bar-nav-icon class="p-0"></v-app-bar-nav-icon>
       </template>
+
+      <v-app-bar-title>MainQUEST<v-icon>mdi-feather</v-icon></v-app-bar-title>
+
+      <template v-slot:append> </template>
     </v-app-bar>
   </div>
 </template>
