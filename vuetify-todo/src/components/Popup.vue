e92132101758b28c3d06240baedb2637da1c93d9
Brandon-Knorr <bknorr@my.wctc.edu>
2025-05-12 11:07:00 -0500

    3.00 popup form to add quest.

--- vuetify-todo/src/components/Popup.vue
+++ vuetify-todo/src/components/Popup.vue
@@ -4,15 +4,22 @@
             <v-col cols="12" md="6">
                 <v-dialog max-width="340">
                     <template v-slot:activator="{ props: activatorProps }">
-                        <v-btn v-bind="activatorProps" prepend-icon="mdi-swords" width="204">
+                        <v-btn v-bind="activatorProps" prepend-icon="mdi-sword-cross" width="204">
                             Add New Quest
                         </v-btn>
                     </template>
 
                     <template v-slot:default="{ isActive }">
-                        <v-card prepend-icon="mdi-swords"
-                            text="When using the activator slot, you must bind the slot props to the activator element."
-                            title="Embark on a new Quest">
+                        <v-card prepend-icon="mdi-sword-cross" title="Embark on a new Quest">
+
+                            <v-card-text>
+                                <v-form class="px-3">
+                                    <v-text-field label="Title" v-model="title"></v-text-field>
+                                    <v-textarea label="Information" v-model="content"></v-textarea>
+                                    <v-btn flat class="success mx-0 mt-3" @click="submit">Add Quest</v-btn>
+                                </v-form>
+                            </v-card-text>
+
                             <template v-slot:actions>
                                 <v-btn class="ml-auto" text="Close" @click="isActive.value = false"></v-btn>
                             </template>
@@ -28,7 +35,13 @@
     export default {
         data() {
             return {
-
+                title: '',
+                content: '',
+            }
+        },
+        methods: {
+            submit() {
+                console.log(this.title, this.content)
             }
         },
     };
