f4ea1db89c06ccccc581b440836bb9ec4fc3500c
Brandon-Knorr <bknorr@my.wctc.edu>
2025-05-19 17:58:57 -0500

    populated data to home page from db. - show icons on tasks that are overdue

--- vuetify-todo/src/pages/Home.vue
+++ vuetify-todo/src/pages/Home.vue
@@ -1,48 +1,53 @@
 <script>
-import Navbar from "@/components/Navbar.vue";
-export default {
-  data() {
-    return {
-      quests: [
-        { title: 'JavaScript Final', due: '05/14/25', status: 'Ongoing', difficulty: 'Hard' },
-        { title: 'Clean Room', due: '05/16/25', status: 'Ongoing', difficulty: 'Medium' },
-        { title: 'Oil Change', due: '06/23/25', status: 'Complete', difficulty: 'Medium' },
-        { title: 'Feed Cat', due: 'N/A', status: 'Ongoing', difficulty: 'Easy' },
-        { title: 'Study Code', due: '05/05/25', status: 'Overdue', difficulty: 'Hard' },
-      ],
-    }
-  },
-  computed: {
-    myQuests() {
-      return this.quests.filter(quest => {
-        return quest.status === 'Overdue' || 'Ongoing'
-      })
-    }
-  }
-};
+  import Navbar from '@/components/Navbar.vue';
+  import { collection, onSnapshot } from 'firebase/firestore';
+  import { db } from '@/firebase/firebase.js';
+  import Task from '@/models/Task.js';
+  export default {
+    data () {
+      return {
+        tasks: [],
+      }
+    },
+    computed: {
+    },
+    mounted () {
+      this.loadTasks();
+    },
+    methods: {
+      loadTasks () {
+        const taskCollection = collection(db, 'Tasks');
+        onSnapshot(taskCollection,
+                   querySnapshot => {
+                     this.tasks = [];
+                     querySnapshot.forEach(doc => {
+                       this.tasks.push(new Task(doc.id, doc.data()))
+                     });
+                   });
+      },
+    },
+  };
 </script>
 
 
 <template>
-    <Navbar/>
-    <h1 class="text-h5 font-weight-light">Home</h1>
+  <Navbar />
+  <h1 class="text-h5 font-weight-light">Home</h1>
 
-    <v-container class="my-5">
-        <v-expansion-panels flat hover>
-            <v-expansion-panel color="#ecdcf9" v-for="quest in myQuests" :key="quest.title">
-                <v-expansion-panel-title disable-icon-rotate>
-                    {{ quest.title }}
-                    <template v-slot:actions>
-                        <v-icon color="#f64740" icon="mdi-alert-circle">
-                        </v-icon>
-                    </template>
-                </v-expansion-panel-title>
-                <v-expansion-panel-text>
-                    <div>due date:</div>
-                    {{ quest.due }}
-                </v-expansion-panel-text>
-            </v-expansion-panel>
-        </v-expansion-panels>
-    </v-container>
+  <v-container class="my-5">
+    <v-expansion-panels flat hover>
+      <v-expansion-panel v-for="task in tasks" :key="task.id" color="#ecdcf9">
+        <v-expansion-panel-title disable-icon-rotate>
+          {{ task.title }}
+          <template #actions>
+            <v-icon v-if="task.status == 'Overdue'" color="#f64740" icon="mdi-alert-circle" />
+          </template>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <div>due date:</div>
+          {{ task.due }}
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
 </template>
-
